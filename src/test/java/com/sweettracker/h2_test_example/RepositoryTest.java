8103821cbdea9ebfa2a54dafd749eba060bf14be
od <od@sweettracker.co.kr>
2024-05-22 16:52:24 +0900

    test: test properties -> test yaml 로 변경

--- src/test/java/com/sweettracker/h2_test_example/RepositoryTest.java
+++ src/test/java/com/sweettracker/h2_test_example/RepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-//@TestPropertySource("classpath:application-test.properties")
 public class RepositoryTest {
 
     @Autowired
